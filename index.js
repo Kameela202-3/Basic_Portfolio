784d03f563c77dd07669957aac304794420ae8aa
Your GitHub Username <your-email.example.com>
2025-07-25 14:06:10 +0100

    initial commit of basic_portfolio

---
+++ index.js
@@ -0,0 +1,8 @@
+// // alert("hey welcome to my website")
+// // prompt("what is your name?");
+// let firstName="kameela";
+// let name = prompt("What is your first name");
+// let namee = prompt("What is your last name");
+// let fullName = name + " " + namee;
+// alert(fullName);
+// console.log(firstName);
